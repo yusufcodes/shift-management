7683d111231664a7a13e036504fded4b89ec5521
yusufcodes <yusufcodes@outlook.com>
2021-01-23 17:59:21 +0000

    Added state, stored shift data in such state, and then outputting basic shift information to dashboard;

--- src/components/ShiftDashboard/ShiftDashboard.tsx
+++ src/components/ShiftDashboard/ShiftDashboard.tsx
@@ -1,5 +1,6 @@
 import { createStyles, makeStyles, Theme, Typography } from '@material-ui/core';
 import * as React from 'react';
+import { useState } from 'react';
 import { useEffect } from 'react';
 
 interface IShiftDashboardProps {}
@@ -12,30 +13,35 @@ const useStyles = makeStyles((theme: Theme) =>
    }),
 );
 
-interface ShiftData {
+interface iShiftData {
+   id: number;
    date: string;
    time: string;
 }
 
 const ShiftDashboard: React.FunctionComponent<IShiftDashboardProps> = () => {
+   const [shiftData, setShiftData] = useState([]);
+
    useEffect(() => {
       fetch('./data.json')
-         .then((response) => {
-            return response.json();
-         })
-         .then((data) => {
-            const shifts = data[0].shifts;
-            shifts.forEach((item: ShiftData) => {
-               console.log(item.date);
-            });
-         });
+         .then((response) => response.json())
+         .then((data) => setShiftData(data[0].shifts));
    });
 
    const classes = useStyles();
    return (
-      <Typography variant={'h3'} className={classes.heading}>
-         Shift Dashboard
-      </Typography>
+      <>
+         <Typography variant={'h3'} className={classes.heading}>
+            Shift Dashboard
+         </Typography>
+         <ul>
+            {shiftData.map((item: iShiftData) => (
+               <li key={item.id}>
+                  {item.date} - {item.time}
+               </li>
+            ))}
+         </ul>
+      </>
    );
 };
 
