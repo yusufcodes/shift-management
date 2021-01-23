c81ae0f975d3fab81c0e867f73a91d68cea7fb34
yusufcodes <yusufcodes@outlook.com>
2021-01-23 17:44:00 +0000

    Read in dummy shift data from json file and output to console

--- src/components/ShiftDashboard/ShiftDashboard.tsx
+++ src/components/ShiftDashboard/ShiftDashboard.tsx
@@ -1,10 +1,42 @@
-import { Typography } from '@material-ui/core';
+import { createStyles, makeStyles, Theme, Typography } from '@material-ui/core';
 import * as React from 'react';
+import { useEffect } from 'react';
 
 interface IShiftDashboardProps {}
 
+const useStyles = makeStyles((theme: Theme) =>
+   createStyles({
+      heading: {
+         textAlign: 'center',
+      },
+   }),
+);
+
+interface ShiftData {
+   date: string;
+   time: string;
+}
+
 const ShiftDashboard: React.FunctionComponent<IShiftDashboardProps> = () => {
-   return <Typography>Shift Dashboard</Typography>;
+   useEffect(() => {
+      fetch('./data.json')
+         .then((response) => {
+            return response.json();
+         })
+         .then((data) => {
+            const shifts = data[0].shifts;
+            shifts.forEach((item: ShiftData) => {
+               console.log(item.date);
+            });
+         });
+   });
+
+   const classes = useStyles();
+   return (
+      <Typography variant={'h3'} className={classes.heading}>
+         Shift Dashboard
+      </Typography>
+   );
 };
 
 export default ShiftDashboard;
